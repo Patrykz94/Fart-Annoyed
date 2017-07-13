547c89dde202c2da4413bcb393d9de11ec50c2da
Patrykz94 <zawpat@googlemail.com>
2017-07-13 21:54:02 +0100

    Added Rect. Get Rect M8

---
+++ Engine/RectF.h
@@ -0,0 +1,22 @@
+/**************************************************************
+	A general rectangle function that will be used by other
+	objects.
+**************************************************************/
+#pragma once
+#include "Vec2.h"
+
+class RectF
+{
+public:
+	RectF() = default;
+	RectF(float left_in, float right_in, float top_in, float bottom_in);
+	RectF(Vec2& topLeft, Vec2& bottomRight);
+	RectF(Vec2& topLeft, float width, float height);
+	bool IsOverlappingWith(RectF& other);
+
+public:
+	float left;
+	float right;
+	float top;
+	float bottom;
+};
