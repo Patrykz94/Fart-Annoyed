5c797665def8840029d60f28e400ae19e6a333f3
Patrykz94 <zawpat@googlemail.com>
2017-07-14 00:14:28 +0100

    Thats paddling

---
+++ Engine/Paddle.cpp
@@ -0,0 +1,49 @@
+#include "Paddle.h"
+
+Paddle::Paddle(Vec2 & pos_in)
+	:
+	pos(pos_in)
+{
+}
+
+void Paddle::Draw(Graphics & gfx)
+{
+	gfx.DrawRect(GetRect(), color);
+}
+
+void Paddle::Update(Keyboard& kbd, float dt)
+{
+	if (kbd.KeyIsPressed(VK_LEFT)) {
+		pos.x -= speed * dt;
+	}
+	else if (kbd.KeyIsPressed(VK_RIGHT)) {
+		pos.x += speed * dt;
+	}
+}
+
+bool Paddle::DoBallCollisions(Ball & ball)
+{
+	if (GetRect().IsOverlappingWith(ball.GetRect()))
+	{
+		ball.ReboundY();
+		return true;
+	}
+	return false;
+}
+
+void Paddle::DoWallCollisions(RectF & walls)
+{
+	if (GetRect().left < walls.left)
+	{
+		pos.x += walls.left - GetRect().left;
+	}
+	else if (GetRect().right > walls.right)
+	{
+		pos.x -= GetRect().right - walls.right;
+	}
+}
+
+RectF Paddle::GetRect()
+{
+	return RectF::FromCenter(pos, halfWidth, halfHeight);
+}
---
+++ Engine/Paddle.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Vec2.h"
+#include "Graphics.h"
+#include "Colors.h"
+#include "Keyboard.h"
+#include "Ball.h"
+
+class Paddle
+{
+public:
+	Paddle(Vec2& pos_in);
+	void Draw(Graphics& gfx);
+	void Update(Keyboard& kbd, float dt);
+	bool DoBallCollisions(Ball& ball);
+	void DoWallCollisions(RectF& walls);
+	RectF GetRect();
+private:
+	const Color color = Colors::White;
+	const float speed = 300.0f;
+	Vec2 pos;
+	const float halfWidth = 50.0f;
+	const float halfHeight = 15.0f;
+};
