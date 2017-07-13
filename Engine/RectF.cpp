a20e4143da761bfb079df8f70c4857c536b2785f
Patrykz94 <zawpat@googlemail.com>
2017-07-13 22:15:36 +0100

    Forgot how to constructors...

--- Engine/RectF.cpp
+++ Engine/RectF.cpp
@@ -1,21 +1,24 @@
 #include "RectF.h"
 
 RectF::RectF(float left_in, float right_in, float top_in, float bottom_in)
+	:
+	left(left_in),
+	right(right_in),
+	top(top_in),
+	bottom(bottom_in)
 {
-	left = left_in;
-	right = right_in;
-	top = top_in;
-	bottom = bottom_in;
 }
 
 RectF::RectF(Vec2& topLeft, Vec2& bottomRight)
+	:
+	RectF(topLeft.x, bottomRight.x, topLeft.y, bottomRight.y)
 {
-	RectF( topLeft.x, bottomRight.x, topLeft.y, bottomRight.y);
 }
 
 RectF::RectF(Vec2& topLeft, float width, float height)
+	:
+	RectF(topLeft, topLeft + Vec2(width, height))
 {
-	RectF( topLeft, topLeft + Vec2( width, height ) );
 }
 
 bool RectF::IsOverlappingWith(RectF& other)
