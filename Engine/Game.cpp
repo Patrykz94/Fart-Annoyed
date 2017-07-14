346057b015443a21f601ad074905ce2b990191d1
Patrykz94 <zawpat@googlemail.com>
2017-07-14 22:10:12 +0100

    Improved simulation time steps. Can simulate multiple times per frame

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -46,15 +46,20 @@ Game::Game( MainWindow& wnd )
 
 void Game::Go()
 {
-	gfx.BeginFrame();	
-	UpdateModel();
+	gfx.BeginFrame();
+	float elapsedTime = ft.Mark();
+	while (elapsedTime > 0.0f)
+	{
+		const float dt = std::min(0.0025f, elapsedTime);
+		UpdateModel(dt);
+		elapsedTime -= dt;
+	}
 	ComposeFrame();
 	gfx.EndFrame();
 }
 
-void Game::UpdateModel()
+void Game::UpdateModel( float dt)
 {
-	const float dt = ft.Mark();
 	ball.Update(dt);
 
 	bool collisionHappened = false;
