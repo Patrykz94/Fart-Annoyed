4cf54d9f49df075d548ceafc50cf60af6d7bc40f
Patrykz94 <zawpat@googlemail.com>
2017-07-20 19:10:24 +0100

    Added heart sprite

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -9,4 +9,5 @@ public:
 	// centered drawing of a sprite
 	static void DrawBall( const Vec2& center,Graphics& gfx );
 	static void DrawGameOver( const Vec2& center, Graphics& gfx );
+	static void DrawHeart(const Vec2& center, Graphics& gfx);
 };
