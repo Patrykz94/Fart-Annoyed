bfd4866d61892bcae642c8bdc2668738b701a813
Patrykz94 <zawpat@googlemail.com>
2017-07-20 18:31:40 +0100

    Resized the paddle

--- Engine/Paddle.h
+++ Engine/Paddle.h
@@ -18,9 +18,9 @@ private:
 	const Color color = Colors::White;
 	const float speed = 300.0f;
 	Vec2 pos;
-	const float halfWidth = 50.0f;
-	const float halfHeight = 15.0f;
-	const float wingsWidth = 15.0f;
+	const float halfWidth = 45.0f;
+	const float halfHeight = 10.0f;
+	const float wingsWidth = 10.0f;
 	const Color wingsColor = Colors::Red;
 	bool isCooldown = false;
 };
