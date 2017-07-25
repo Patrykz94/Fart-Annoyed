175705819cc7af376e349b462ba2439a339b4d8c
Patrykz94 <zawpat@googlemail.com>
2017-07-25 21:20:34 +0100

    Added multiple new sprites

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -6,8 +6,24 @@
 class SpriteCodex
 {
 public:
-	// centered drawing of a sprite
-	static void DrawBall( const Vec2& center,Graphics& gfx );
-	static void DrawGameOver( const Vec2& center, Graphics& gfx );
-	static void DrawHeart(const Vec2& center, Graphics& gfx);
+	// drawing of a sprite
+	static void DrawBall(const Vec2& center,Graphics& gfx);
+	static void DrawDead(const Vec2& center, Graphics& gfx);
+	static void DrawTitle(const Vec2& center, Graphics& gfx);
+	static void DrawGameOver(const Vec2& center, Graphics& gfx);
+	static void DrawHeart(const Vec2& topLeft, Graphics& gfx);
+	static void DrawTextScore(const Vec2& topLeft, Graphics& gfx);
+	static void DrawNumberScore(const Vec2& topLeft, Graphics& gfx, const int& score);
+
+	// drawing number sprites 14px x 14px
+	static void DrawNumber0(const Vec2& topLeft, Graphics& gfx);
+	static void DrawNumber1(const Vec2& topLeft, Graphics& gfx);
+	static void DrawNumber2(const Vec2& topLeft, Graphics& gfx);
+	static void DrawNumber3(const Vec2& topLeft, Graphics& gfx);
+	static void DrawNumber4(const Vec2& topLeft, Graphics& gfx);
+	static void DrawNumber5(const Vec2& topLeft, Graphics& gfx);
+	static void DrawNumber6(const Vec2& topLeft, Graphics& gfx);
+	static void DrawNumber7(const Vec2& topLeft, Graphics& gfx);
+	static void DrawNumber8(const Vec2& topLeft, Graphics& gfx);
+	static void DrawNumber9(const Vec2& topLeft, Graphics& gfx);
 };
